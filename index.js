a25a49d69cc33faa6f010f9fc2685cd2627e2f1d
Grayson Germsheid <grayson.germsheid@edu.sait.ca>
2023-06-11 13:54:37 -0600

    fixed typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const postres = require("pg");
-const path    = require("path");
-const port    = process.env.PORT || 3000;
+const express   = require("express");
+const postgres  = require("pg");
+const path      = require("path");
+const port      = process.env.PORT || 3000;
 
 const dbh = postgres.Client(
 {
