eaff9fe73b8fee8951216179366457d6d9df0c6d
Grayson Germsheid <grayson.germsheid@edu.sait.ca>
2023-06-11 22:25:38 -0600

    App should restart on update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     apps : [
     {
-        name   : "travelexperts",
-        script : "npm run sync && npm run start"
+        name:   "travelexperts",
+        script: "npm run sync && npm run start",
+        watch:  true,
+        ignore_watch: ["node_modules", "media"]
     }]
 }
