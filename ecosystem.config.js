1b396501aaf4c4d00c2791d80fe5270d23749e06
Grayson Germsheid <grayson.germsheid@edu.sait.ca>
2023-06-12 12:56:08 -0600

    server config fix? maybe?

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     {
         name:   "travelexperts",
         script: "npm run sync && npm run start",
+        watch_delay: 1000,
         watch:  true,
         ignore_watch: ["node_modules", "media"]
     }]
