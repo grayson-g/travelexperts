5efb6a7b61e33801345683617a466836e70fed6e
Grayson Germsheid <grayson.germsheid@edu.sait.ca>
2023-06-11 22:22:36 -0600

    test2

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mysql     = require("mysql2");
 const path      = require("path");
 const port      = process.env.PORT || 3000;
 
-
 const app       = express();
 
 const dbc       = mysql.createPool({
