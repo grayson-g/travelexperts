f2a2d2de2282ddc39258b949e763c15608326d6f
Grayson Germsheid <grayson.germsheid@edu.sait.ca>
2023-06-12 13:04:45 -0600

    Nevermind ignore last commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [
     {
         name:   "travelexperts",
-        script: "npm run sync && npm run start",
+        script: "npm run start",
         watch_delay: 1000,
         watch:  true,
         ignore_watch: ["node_modules", "media"]
